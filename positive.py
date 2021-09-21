7c4e6601beee1d2c0bcd23f295b2f5f7352e6b49
SheikLaisha <90794575+SheikLaisha@users.noreply.github.com>
2021-09-21 19:44:51 +0530

    Create positive.py

---
+++ positive.py
@@ -0,0 +1,11 @@
+list1 = [12,-7,5,64,-14]
+list2 = [12,14,-95,3]
+print('The positive numbers in list1 are :')
+for x in list1:
+    if x >= 0:
+        print(x,end=' ')
+print('\nThe positive numbers in list2 are :')
+for x in list2:
+    if x >= 0:
+        print(x,end=' ')
+        
